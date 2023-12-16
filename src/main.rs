44374c310c47811b3cd8e02520c8f8742e4f7710
Nick Calibey <nick.calibey@gmail.com>
2023-12-16 11:21:15 -0600

    Implement VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,25 @@
 #![no_std]  // Don't link the Rust standard library.
 #![no_main] // Disable all Rust-level entry points.
 
+use core::fmt::Write;
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
 
+mod vga_buffer;
+
+// This function is the entry point, since the Linker looks for a function
+// name `_start` by default.
 #[no_mangle] // Don't mangle the name of this function.
 pub extern "C" fn _start() -> ! {
-    // This function is the entry point, since the Linker looks for a function
-    // name `_start` by default.
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
 
 /// Call this function on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
