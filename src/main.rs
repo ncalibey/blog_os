ac335da54d457db6fe93be2d0a5c7da3c932f83a
Nick Calibey <nick.calibey@gmail.com>
2023-12-18 03:57:48 -0600

    Setup testing

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,34 @@
 #![no_std]  // Don't link the Rust standard library.
 #![no_main] // Disable all Rust-level entry points.
+#![reexport_test_harness_main = "test_main"]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
 
-use core::fmt::Write;
+use blog_os::println;
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World!";
-
-mod vga_buffer;
 
 // This function is the entry point, since the Linker looks for a function
 // name `_start` by default.
 #[no_mangle] // Don't mangle the name of this function.
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 /// Call this function on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
 }
